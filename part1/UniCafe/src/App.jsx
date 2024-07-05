3324dc1caa85bd8b36127b1f27df8e3f9352ed5d
Daniil Parniukov <dparniukov@gmail.com>
2024-07-05 19:08:52 +0300

    exercise 1.7 done

--- part1/UniCafe/src/App.jsx
+++ part1/UniCafe/src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from "react"
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Statistic = ({ title, number }) => (
-  <p>
-    {title} {number}
-  </p>
+const Statistic = ({ title, statistic }) => (
+  <>
+    {title} {statistic} <br />
+  </>
 )
 
 const App = () => {
@@ -18,6 +18,7 @@ const App = () => {
   const handleNeutralFeedback = () => setNeutral(neutral + 1)
   const handleBadFeedback = () => setBad(bad + 1)
 
+  const total = good + neutral + bad
   return (
     <div>
       <h1>give feedback</h1>
@@ -25,9 +26,18 @@ const App = () => {
       <Button onClick={handleNeutralFeedback} text="neutral" />
       <Button onClick={handleBadFeedback} text="bad" />
       <h1> statistics </h1>
-      <Statistic title="good" number={good} />
-      <Statistic title="neutral" number={neutral} />
-      <Statistic title="bad" number={bad} />
+      <Statistic title="good" statistic={good} />
+      <Statistic title="neutral" statistic={neutral} />
+      <Statistic title="bad" statistic={bad} />
+      <Statistic title="all" statistic={total} />
+      <Statistic
+        title="average"
+        statistic={((good - bad) / total).toFixed(3)}
+      />
+      <Statistic
+        title="positive"
+        statistic={((good * 100) / total).toFixed(3).toString() + "%"}
+      />
     </div>
   )
 }
