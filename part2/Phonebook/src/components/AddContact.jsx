b5a6587ffb4a75336bba36009c52f5190ab5c081
Daniil Parniukov <dparniukov@gmail.com>
2024-07-19 03:37:24 +0300

    minor changes so frontend works well with the backend

--- part2/Phonebook/src/components/AddContact.jsx
+++ part2/Phonebook/src/components/AddContact.jsx
@@ -1,115 +1,123 @@
 import contactService from "../services/contacts"
-import { useState } from "react"
-import { useRef } from "react"
+import {useState} from "react"
+import {useRef} from "react"
 
 const AddContact = ({
-  contacts,
-  setContacts,
-  setNotification,
-  setNotificationType,
-}) => {
-  // const [currentName, setCurrentName] = useState("")
-  // const [currentNumber, setCurrentNumber] = useState("")
-  const currentName = useRef("")
-  const currentNumber = useRef("")
-  const setCurrentName = (newName) => (currentName.current = newName)
-  const setCurrentNumber = (newNumber) => (currentNumber.current = newNumber)
+                        contacts,
+                        setContacts,
+                        setNotification,
+                        setNotificationType,
+                    }) => {
+    // const [currentName, setCurrentName] = useState("")
+    // const [currentNumber, setCurrentNumber] = useState("")
+    const currentName = useRef("")
+    const currentNumber = useRef("")
+    const setCurrentName = (newName) => (currentName.current = newName)
+    const setCurrentNumber = (newNumber) => (currentNumber.current = newNumber)
 
-  const handleNameChange = (event) => {
-    setCurrentName(event.target.value)
-    console.log("name is changed", event.target.value, currentName.current)
-  }
-
-  const handleNumberChange = (event) => {
-    setCurrentNumber(event.target.value)
-    console.log("current number is now", currentNumber.current)
-  }
-
-  const addName = (event) => {
-    event.preventDefault()
-    const newContact = {
-      name: currentName.current,
-      number: currentNumber.current,
+    const handleNameChange = (event) => {
+        setCurrentName(event.target.value)
+        console.log("name is changed", event.target.value, currentName.current)
     }
-    console.log(newContact)
 
-    var inList = false
-    // TODO: bad check since there might be a contact like that added in another browser and then ther will be two contacts with the same name.
-    if (contacts.map((contact) => contact.name).includes(newContact.name)) {
-      inList = true
-      if (
-        !window.confirm(
-          `${newContact.name} is already in the contact list. Replace the old number with a new one?`
-        )
-      ) {
-        return
-      }
+    const handleNumberChange = (event) => {
+        setCurrentNumber(event.target.value)
+        console.log("current number is now", currentNumber.current)
     }
-    console.log(currentNumber)
 
-    // ensure that the version in the browser corresponds to the version on server
-    if (inList) {
-      console.log(
-        `the number for ${newContact.name} is being replaced (to ${newContact.number})`
-      )
-      contactService
-        .changeContact(
-          contacts.filter((c) => c.name === newContact.name)[0].id, // find the id of the contact with that name
-          newContact
-        )
-        .then((added) => {
-          console.log(
-            contacts.map((c) => (c.name === newContact.name ? added : c))
-          )
-          setContacts(
-            contacts.map((c) => (c.name === newContact.name ? added : c))
-          )
-          setNotification(
-            `The number of ${newContact.name} is set to ${newContact.number}`
-          )
-        })
-        .catch((error) => {
-          console.log(error)
-          setNotification(
-            `Error! Looks like the information about ${newContact.name} has already been deleted from the server.`
-          )
-          setNotificationType("error")
-          setContacts(contacts.filter((c) => c.name !== newContact.name)) // name is assumed to be unique
-        })
-    } else {
-      console.log(newContact.name, "is being added")
-      contactService
-        .addContact(newContact)
-        .then((added) => setContacts(contacts.concat(added)))
-      setNotification(`${newContact.name} is added.`)
+    const addName = (event) => {
+        event.preventDefault()
+        const newContact = {
+            name: currentName.current,
+            number: currentNumber.current,
+        }
+        console.log(newContact)
+
+        var inList = false
+        // TODO: bad check since there might be a contact like that added in another browser and then ther will be two contacts with the same name.
+        if (contacts.map((contact) => contact.name).includes(newContact.name)) {
+            inList = true
+            if (
+                !window.confirm(
+                    `${newContact.name} is already in the contact list. Replace the old number with a new one?`
+                )
+            ) {
+                return
+            }
+        }
+        console.log(currentNumber)
+
+        // ensure that the version in the browser corresponds to the version on server
+        if (inList) {
+            console.log(
+                `the number for ${newContact.name} is being replaced (to ${newContact.number})`
+            )
+            contactService
+                .changeContact(
+                    contacts.filter((c) => c.name === newContact.name)[0].id, // find the id of the contact with that name
+                    newContact
+                )
+                .then((added) => {
+                    console.log(
+                        contacts.map((c) => (c.name === newContact.name ? added : c))
+                    )
+                    setContacts(
+                        contacts.map((c) => (c.name === newContact.name ? added : c))
+                    )
+                    setNotification(
+                        `The number of ${newContact.name} is set to ${newContact.number}`
+                    )
+                })
+                .catch((error) => {
+                    console.log(error)
+                    setNotification(
+                        `Error! Looks like the information about ${newContact.name} has already been deleted from the server.`
+                    )
+                    setNotificationType('error')
+                    setContacts(contacts.filter((c) => c.name !== newContact.name)) // name is assumed to be unique
+                })
+        } else {
+            console.log(newContact.name, "is being added")
+            contactService
+                .addContact(newContact)
+                .then((added) => {
+                    setContacts(contacts.concat(added))
+                    setNotification(`${newContact.name} is added.`)
+                })
+                .catch(error => {
+                    console.log(error)
+                    setNotification(`${error.response.data.error}`)
+                    setNotificationType('error')
+                })
+
+        }
+        console.log("got here")
+
+        setTimeout(() => {
+            setNotification(null)
+            setNotificationType("success") // the default type is set to success; bad code since strings are hardcoded
+        }, 5000) // fix: bad code as this may ruin future notifications
+        event.target.reset()
+        // rerenders automatically as contacts are set
     }
-    console.log("got here")
 
-    setTimeout(() => {
-      setNotification(null)
-      setNotificationType("success") // the default type is set to success; bad code since strings are hardcoded
-    }, 5000)
-    event.target.reset()
-    // rerenders automatically as contacts are set
-  }
-
-  return (
-    <form
-      onSubmit={addName}
-      onReset={() => {
-        setCurrentName("")
-        setCurrentNumber("")
-      }}
-    >
-      <div>
-        name: <input onInput={handleNameChange} />
-      </div>
-      <div>
-        number: <input onInput={handleNumberChange} />
-      </div>
-      <button type="submit">add</button>
-    </form>
-  )
+    return (
+        <form
+            onSubmit={addName}
+            onReset={() => {
+                setCurrentName("")
+                setCurrentNumber("")
+            }}
+        >
+            <div>
+                name: <input onInput={handleNameChange}/>
+            </div>
+            <div>
+                number: <input onInput={handleNumberChange}/>
+            </div>
+            <button type="submit">add</button>
+        </form>
+    )
 }
 
 export default AddContact
