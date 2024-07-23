75631c754c6b09044ede36d7ce07b2431856e9da
Daniil Parniukov <dparniukov@gmail.com>
2024-07-23 16:00:40 +0300

    added eslint.

---
+++ part5/bloglist-frontend/eslint.config.js
@@ -0,0 +1,57 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import stylistic from '@stylistic/eslint-plugin'
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@stylistic/indent': [
+        'error',
+        4
+      ],
+      '@stylistic/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/semi': [
+        'error',
+        'never'
+      ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', {'before': true, 'after': true},
+      ],
+      'no-console': 'off',
+      'no-unused-vars': [
+        "error",
+        {
+          "argsIgnorePattern": "^_", // because sometimes you have to not use a var.
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_"
+        }
+      ]
+    },
+  },
+  {
+    ignores: ["dist/**", "build/**"],
+  }
+];
