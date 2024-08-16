dfe3600150ff9e162c64988da8888714ac4f8e5a
Daniil Parniukov <dparniukov@gmail.com>
2024-08-16 12:59:31 +0300

    added id generation for comments

--- part4/BlogList/controllers/comments.js
+++ part4/BlogList/controllers/comments.js
@@ -1,12 +1,13 @@
 const commentRouter = require('express').Router()
 const Blog = require('../models/blog')
+const uuid = require('uuid')
 
 
 commentRouter.post('/:id/comments/', async (req, resp) => {
     const blogId = req.params.id
 
     const currentComments = (await Blog.findById(blogId)).comments
-    const updatedBlog = await Blog.findByIdAndUpdate(blogId, { comments: currentComments.concat(req.body.comment) }, { new: true })
+    const updatedBlog = await Blog.findByIdAndUpdate(blogId, { comments: currentComments.concat({ body: req.body.comment, id: uuid.v4() }) }, { new: true })
 
     resp.json(updatedBlog)
 })
